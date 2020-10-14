78c4670eaf62d38df72623f03bdbb956ff0ed1f2
Marcos Vinicius <marcos_andrade9@hotmail.com>
2020-10-13 22:31:58 -0300

    Connecting to mongoDB with repository and service

---
+++ src/main/java/com/marcosandrade/workshopmongo/services/UserService.java
@@ -0,0 +1,25 @@
+package com.marcosandrade.workshopmongo.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.marcosandrade.workshopmongo.domain.User;
+import com.marcosandrade.workshopmongo.repository.UserRepository;
+
+@Service
+public class UserService {
+
+	
+	@Autowired
+	private UserRepository repo;
+	
+	public List<User> findAll() {
+		return repo.findAll();
+		
+		
+		
+	}
+	
+}
