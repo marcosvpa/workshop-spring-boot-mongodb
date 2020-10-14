647edd5cf3c7b528aea7a15aefb19eef42f67152
Marcos Vinicius <marcos_andrade9@hotmail.com>
2020-10-13 22:41:29 -0300

    Database instantiation operation

---
+++ src/main/java/com/marcosandrade/workshopmongo/config/Instantiation.java
@@ -0,0 +1,33 @@
+package com.marcosandrade.workshopmongo.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.marcosandrade.workshopmongo.domain.User;
+import com.marcosandrade.workshopmongo.repository.UserRepository;
+
+@Configuration
+public class Instantiation implements CommandLineRunner{
+
+	@Autowired
+	private UserRepository userReposiroty;
+	
+	
+	@Override
+	public void run(String... args) throws Exception {
+		userReposiroty.deleteAll();
+		
+		User maria = new User(null, "Maria Brown", "maria@gmail.com");
+		User alex = new User(null, "Alex Green", "alex@gmail.com");
+		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+		
+		userReposiroty.saveAll(Arrays.asList(maria, alex, bob));
+		
+	}
+
+	
+	
+}
