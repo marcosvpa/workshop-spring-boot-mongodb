6fde285561d7d9efc24edbe17ba18195fe8e8a23
Marcos Vinicius <marcos_andrade9@hotmail.com>
2020-10-20 22:52:40 -0300

    Returning endpoint of user posts

--- src/main/java/com/marcosandrade/workshopmongo/resources/UserResource.java
+++ src/main/java/com/marcosandrade/workshopmongo/resources/UserResource.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.marcosandrade.workshopmongo.domain.Post;
 import com.marcosandrade.workshopmongo.domain.User;
 import com.marcosandrade.workshopmongo.dto.UserDTO;
 import com.marcosandrade.workshopmongo.services.UserService;
@@ -36,6 +37,13 @@ public class UserResource {
 		User obj = service.findById(id);
 		return ResponseEntity.ok().body(new UserDTO(obj));
 	}
+	
+	@RequestMapping(value ="/{id}/posts",method = RequestMethod.GET)
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
+	}
+	
 	
 	@RequestMapping(method = RequestMethod.POST)
 	public ResponseEntity<Void> insert(@RequestBody UserDTO objDTO) {
