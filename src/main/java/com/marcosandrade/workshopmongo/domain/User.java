aab2d3a6843695b58b67f49fa72fadf9a51e8451
Marcos Vinicius <marcos_andrade9@hotmail.com>
2020-10-20 22:47:18 -0300

    Referencing post of user

--- src/main/java/com/marcosandrade/workshopmongo/domain/User.java
+++ src/main/java/com/marcosandrade/workshopmongo/domain/User.java
@@ -1,8 +1,11 @@
 package com.marcosandrade.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 
@@ -11,10 +14,21 @@ public class User implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 	
+	public List<Post> getPosts() {
+		return posts;
+	}
+
+	public void setPosts(List<Post> posts) {
+		this.posts = posts;
+	}
+
 	@Id
 	private String id;
 	private String name;
 	private String email;
+	
+	@DBRef(lazy = true)
+	private List<Post> posts = new ArrayList<>();
 
 	public User() {
 	}
